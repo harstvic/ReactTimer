dcfdda5e783a7e60e13c451fdaf1306b214b9679
Victor Harstookov <harstvic@gmail.com>
2016-12-11 17:53:01 +0200

    Init commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+var Main = (props) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Main.jsx Rendered</p>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Main;
